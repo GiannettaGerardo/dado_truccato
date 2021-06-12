9e1b522c87e4db9f343e7dff9c3ea72a87498eed
GiannettaGerardo <ggp00519@protonmail.com>
2021-06-12 19:19:02 +0200

    Creare la funzione per il controllo degli NPj >= 5 #1 Controllo implementato

--- dado_truccato.py
+++ dado_truccato.py
@@ -21,13 +21,11 @@ def calcoloSingoleFrequenze(risultatiSingoliLanci):
     return frequenzeAssolute
 
 
-# fa : frequenze assolute
-def calcoloNPj(fa)
+# n : totale frequenze assolute
+def calcoloNPj(n):
     np = []
-    for i in range(1:7):
-        npj = fa[i]*(1/6)
-        subList = [npj, npj >= 5]
-        np.append(subList)
+    for i in range(6):
+        np.append(n * (1/6))
     return np
 
 
@@ -51,6 +49,14 @@ if __name__ == '__main__':
     
     print(frequenzeAssolute) # debug
 
-    for frequenza in frequenzeAssolute[1:6]:
+    for frequenza in frequenzeAssolute[1:7]:
         totaleFrequenzeAssolute += frequenza
     print(totaleFrequenzeAssolute) # debug
+    
+    np = calcoloNPj(totaleFrequenzeAssolute)
+    print(np) # debug
+
+    if (np[0] < 5):
+        print('numero di tentativi troppo basso')
+        exit()
+
